742d28d61f5db8b3b9bdb37607d213aa44288615
nevtay <nevintay@gmail.com>
2020-05-01 22:34:58 +0800

    Add CRUD routes

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 
+const createRoute = require('./routes/create.route')
+const readRoute = require('./routes/todos.route')
+const updateRoute = require('./routes/update.route')
+const deleteRoute = require('./routes/delete.route')
+
 app.get('/', (req, res) => {
   res.send(
         `
@@ -17,5 +22,10 @@ app.get('/', (req, res) => {
         `
   )
 })
+
+app.use('/create', createRoute)
+app.use('/todos', readRoute)
+// app.use('/update', updateRoute)
+// app.use('/delete', deleteRoute)
 
 module.exports = app
