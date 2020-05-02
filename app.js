61371525fade3e71dcf56d9ac125645acf5f5116
nevtay <nevintay@gmail.com>
2020-05-02 21:42:07 +0800

    Add delete route; add port 3000 to corsOptions

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const express = require('express')
 const app = express()
 
 const cors = require('cors')
+const corsOptions = {
+  origin: ['*', 3000],
+  optionsSuccessStatus: 200
+}
 
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json())
 
 const createRoute = require('./routes/create.route')
 const readRoute = require('./routes/todos.route')
 // const updateRoute = require('./routes/update.route')
-// const deleteRoute = require('./routes/delete.route')
+const deleteRoute = require('./routes/delete.route')
 
 app.get('/', (req, res) => {
   res.send(
@@ -26,6 +30,6 @@ app.get('/', (req, res) => {
 app.use('/create', createRoute)
 app.use('/todos', readRoute)
 // app.use('/update', updateRoute)
-// app.use('/delete', deleteRoute)
+app.use('/delete', deleteRoute)
 
 module.exports = app
