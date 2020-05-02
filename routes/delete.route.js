e825ff4f7af0eb18fa9453e6f83a2fccb3ee61ed
nevtay <nevintay@gmail.com>
2020-05-02 21:42:33 +0800

    Add DELETE request functionality

--- routes/delete.route.js
+++ routes/delete.route.js
@@ -2,4 +2,17 @@ const express = require('express')
 const router = express.Router()
 const Todo = require('../models/Todo.model')
 
+router.delete('/:todoId', async (req, res) => {
+  const deletedTodo = await Todo.findOneAndRemove({ id: req.params.todoId },
+    (err, data) => {
+      if (err) {
+        res.send(err)
+      } else {
+        res
+          .status(200)
+          .send(data)
+      }
+    })
+})
+
 module.exports = router
