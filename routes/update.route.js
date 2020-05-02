fd77fb3ee070dbe839f07e1cff6901d8d978b282
nevtay <nevintay@gmail.com>
2020-05-02 23:33:42 +0800

    Create update functionality

--- routes/update.route.js
+++ routes/update.route.js
@@ -2,4 +2,17 @@ const express = require('express')
 const router = express.Router()
 const Todo = require('../models/Todo.model')
 
+router.post('/', async (req, res) => {
+  const { id, label } = { ...req.body }
+  Todo.findOneAndUpdate({ id: id }, { label: label }, (err, data) => {
+    if (err) {
+      res.send(err)
+    } else {
+      res
+        .status(200)
+        .send(data)
+    }
+  })
+})
+
 module.exports = router
