961aa3d76bd8fb5be5e1889fe4bee0583e0c8571
nevtay <nevintay@gmail.com>
2020-05-03 23:05:28 +0800

    Remove const variable as it offered no beneficial use; change payload sent on successfully deletion

--- routes/delete.route.js
+++ routes/delete.route.js
@@ -3,14 +3,14 @@ const router = express.Router()
 const Todo = require('../models/Todo.model')
 
 router.delete('/:todoId', async (req, res) => {
-  const deletedTodo = await Todo.findOneAndRemove({ id: req.params.todoId },
+  await Todo.findOneAndRemove({ id: req.params.todoId },
     (err, data) => {
       if (err) {
         res.send(err)
       } else {
         res
           .status(200)
-          .send(data)
+          .send('Todo has been deleted')
       }
     })
 })
